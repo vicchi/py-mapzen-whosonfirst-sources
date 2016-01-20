e3d0b057ba02e97db4837bc375ab0a5aea660e17
Dan Phiffer <dan@phiffer.org>
2016-01-19 16:16:43 -0800

    🔧 Added VERSION to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.sources.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+desc = open("README.md").read()
+version = open("VERSION").read()
 
 setup(
     name='mapzen.whosonfirst.sources',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.sources'],
-    version='0.03',
+    version=version,
     description='Simple Python wrapper for managing Who\'s On First sources',
     author='Mapzen',
     url='https://github.com/whosonfirst/py-mapzen-whosonfirst-sources',
@@ -19,5 +31,5 @@ setup(
     packages=packages,
     scripts=[
         ],
-    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-sources/releases/tag/v0.03',
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-sources/releases/tag/' + version,
     license='BSD')
